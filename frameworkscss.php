c677021e8ddff41a23f089dac141e258ea5fabd9
Metalhead <joseguadaluperamirezscott@gmail.com>
2018-09-20 15:19:38 -0500

    modificacion de script frameworks css

--- frameworkscss.php
+++ frameworkscss.php
@@ -55,71 +55,92 @@
 <!--1.pure css-->
 <h1>Frameworks De Css</h1>
 </br>
-<p>
 <h1>1.-Pure Css</h1>
 </br>
+<img src="imagenes/pure.PNG">
+</br>
+<p>
 Pure css Pesa únicamente 3.8 kb comprimido, ¡pero está repleto de características! Pure CSS es actualmente mi framework CSS favorito desde hace poco más de un año, y un framework a tener en cuenta. Ha sido desarrollado teniendo a los dispositivos móviles en mente, por lo que es ideal si quieres crear un sitio web responsive.
 </p>
 <!--2.Beauter-->
 </br>
-<p>
 <h1>2.-Beauter</h1>
 </br>
+<img src="imagenes/beauter.PNG">
+</br>
+<p>
 Aún no he tenido la oportunidad de probar Beauter en un proyecto, pero os digo que no puedo esperar. Este pequeño (alrededor de 4kb comprimido) framework CSS no solo te proporciona las herramientas necesarias para acelerar el desarrollo de un front-end, sino que diseñar webs con este framework será coser y cantar.
 </p>
 <!--3.mini.css-->
 </br>
-<p>
 <h1>3.-Mini.css</h1>
 </br>
+<img src="imagenes/imagen-varios9.JPG">
+</br>
+<p>
 Un ligero framework (7kb comprimido) para desarrollar webs rápidas de forma sencilla.
 </p>
 <!--4.Milligram-->
 </br>
-<p>
 <h1>4.-Milligram</h1>
 </br>
+<img src="imagenes/milli.PNG">
+</br>
+<p>
 Milligram te proporciona un conjunto de estilos para que tengas un punto de inicio limpio y puedas desarrollar de manera productiva. Solo pesa 2kb comprimido, pero no lo subestimes por su tamaño ya que es un framework super potente. Yo solo situaría dentro del top 3 de todos los frameworks CSS que te mostramos en este artículo.
 </p>
 <!--5.Dead Simple Grid-->
 </br>
-<p>
 <h1>5.-Dead Simple Grid</h1>
 </br>
+<img src="imagenes/grid.PNG">
+</br>
+<p>
 Técnicamente no es un framework, es un grid layout super simple que puedes utilizar en todos tus proyectos. Son solo 250 bytes de código CSS.</p>
 <!--6.Siimple-->
 </br>
-<p>
 <h1>6.-Siimple</h1>
 </br>
+<img src="imagenes/simple.JPG">
+</br>
+<p>
 Siimple es un framework CSS super pequeño para diseños limpios y flat. Es muy ligero y cuenta con características y elementos absolutamente alucinantes.
 </p>
 <!--7.-->
 </br>
-<p>
 <h1>7.-Min</h1>
 </br>
+<img src="imagenes/grid.PNG">
+</br>
+
+<p>
 Framework super ligero que tiene la particularidad de ser compatible con los navegadores más antiguos como IE 5.5 y Opera 9.
 </p>
 <!--8.-->
 </br>
-<p>
 <h1>8.-Lotus</h1>
 </br>
+<img src="imagenes/grid.PNG">
+</br>
+<p>
 Framework muy ligero (1.8kb comprimido) que cuenta con todo lo que necesitas para diseñar un sitio web: grid, media queries, formularios y mucho más.
 </p>
 <!--9.-->
 </br>
-<p>
 <h1>9.-Skeleton</h1>
 </br>
+<img src="imagenes/grid.PNG">
+</br>
+<p>
 Con únicamente 400 líneas de código, Skeleton es ligero como una pluma. Y no solo eso, cuenta con todo lo que puedas necesitar, como grid, tipografía, formularios, media queries... Es decir, no tendrás que insertar nada externo en tu proyecto, con Skeleton te bastará y sobrará.
 </p>
 <!--10.-->
 </br>
-<p>
 <h1>10.-Panic Css</h1>
 </br>
+<img src="imagenes/grid.PNG">
+</br>
+<p>
 Con solo 3Kb ya comprinmido, Picnic es un framework super ligero, pero también tiene todo lo necesario para crear sitios web con aspecto profesional y sobre todo, funcionales.
 </p>
 </br>
